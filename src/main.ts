5dd40d7c6d90090753b489766b8ddfe9d241625f
Fariz101 <farizalghaniputra2010@gmail.com>
2026-05-24 19:48:08 +0700

    fix: force dynamic cors policy for localhost

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Di production, Anda bisa mengganti '*' menjadi URL Vercel Anda demi keamanan
+    origin: true, // Mengizinkan origin apa pun secara dinamis (termasuk localhost:3000)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
+    optionsSuccessStatus: 204, // Mengembalikan status sukses 204 untuk Preflight OPTIONS
   });
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
