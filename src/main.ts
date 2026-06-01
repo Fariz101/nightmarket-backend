13f4803fa610d541c8412e655cdcc075b00e7ceb
Fariz101 <farizalghaniputra2010@gmail.com>
2026-06-01 23:27:51 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-// backend-nestjs/src/main.ts
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Sesuaikan konfigurasi CORS Anda seperti ini
+  
+  // Mengaktifkan CORS agar Next.js Anda bisa menembak API ini
   app.enableCors({
-    origin: 'http://localhost:3001', // URL Aplikasi Next.js Anda
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization, app-key', // 🔑 Tambahkan 'app-key' di sini!
+    origin: '*', // Bisa diperketat menggunakan URL Next.js production Anda nanti
     credentials: true,
   });
 
-  await app.listen(3000);
+  // 🔴 UBAH BAGIAN INI: Wajib dengerin process.env.PORT dari Railway
+  const port = process.env.PORT || 5001; 
+  await app.listen(port, '0.0.0.0'); // Tambahkan '0.0.0.0' agar bisa diikat oleh router internal Railway
+  
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
