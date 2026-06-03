5c5b7d5fac2af1e02b09c930d53f658449a41469
Fariz101 <farizalghaniputra2010@gmail.com>
2026-06-03 07:11:01 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
   origin: [
     'http://localhost:3000',
-    'https://night-market-sooty.vercel.app/'
+    'https://night-market-sooty.vercel.app'
   ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
